ffde52d70d388e531006adec270007ded59ed6d0
Ygor Henrique <rick.ygor@live.com>
2025-06-23 18:20:18 +0200

    add treatment duration

--- InfomedicsPortal/InfomedicsPortal.Core/Treatments/Treatment.cs
+++ InfomedicsPortal/InfomedicsPortal.Core/Treatments/Treatment.cs
@@ -4,4 +4,5 @@ public class Treatment
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public int DurationMins { get; set; }
 }
--- InfomedicsPortal/InfomedicsPortal.Infrastructure/InMemory/TreatmentsRepository.cs
+++ InfomedicsPortal/InfomedicsPortal.Infrastructure/InMemory/TreatmentsRepository.cs
@@ -8,10 +8,10 @@ public class TreatmentsRepository : BaseRepository<Treatment, Guid>, ITreatments
     private static readonly ConcurrentDictionary<Guid, Treatment> Treatments = new(
     new[]
     {
-        new Treatment { Id = Guid.NewGuid(), Name = "Teeth Whitening" },
-        new Treatment { Id = Guid.NewGuid(), Name = "Dental Crown" },
-        new Treatment { Id = Guid.NewGuid(), Name = "Root Canal Therapy" },
-        new Treatment { Id = Guid.NewGuid(), Name = "Orthodontic Braces" }
+        new Treatment { Id = Guid.NewGuid(), Name = "Teeth Whitening", DurationMins = 60 },
+        new Treatment { Id = Guid.NewGuid(), Name = "Dental Crown", DurationMins = 90 },
+        new Treatment { Id = Guid.NewGuid(), Name = "Root Canal Therapy", DurationMins = 90 },
+        new Treatment { Id = Guid.NewGuid(), Name = "Orthodontic Braces", DurationMins = 90 }
     }.ToDictionary(t => t.Id));
     
     public TreatmentsRepository() : base(
